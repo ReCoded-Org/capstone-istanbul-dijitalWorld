89c9225358611aa10ca3960bf6da02f3cd6195e1
refica <refia.candan@gmail.com>
2020-07-19 18:41:29 +0300

    created basic folders and files

---
+++ src/components/newsletter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function newsletter() {
+  return (
+    <div className="newsletter">
+    
+    </div>
+  );
+}
+
+export default newsletter;
---
+++ src/containers/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Newsletter from "../components/newsletter";
+
+function Home() {
+  return (
+    <div className="home">
+        <Newsletter />
+    </div>
+  );
+}
+
+export default Home;
