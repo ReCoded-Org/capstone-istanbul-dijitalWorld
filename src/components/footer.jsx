62508a138317428feb75975e4b1ece4db5c1490b
refica <refia.candan@gmail.com>
2020-07-19 18:38:05 +0300

    created basic files and folders

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function footer() {
+  return (
+    <div className="footer">
+    
+    </div>
+  );
+}
+
+export default footer;
---
+++ src/containers/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from "../components/footer";
+
+function Home() {
+  return (
+    <div className="home">
+        <Footer />
+    </div>
+  );
+}
+
+export default Home;
