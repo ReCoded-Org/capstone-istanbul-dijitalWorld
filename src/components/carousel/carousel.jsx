460733216160788c2ba6532b7626b569e6497f35
Your NameDara-D <dara.dandashi@gmail.com>
2020-07-19 14:11:53 +0300

    added the carousel draft

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -17,7 +17,34 @@ export default function carousel() {
           <button className="learnBtn">Learn More</button>
         </div>
       </div>
-      
+      <div className="rightPart">
+        <div className="calouselTrackContainer">
+          <ul className="carousleTrack">
+            <li className="carouselSlide">
+              <img
+                className="carouselImg"
+                src="https://static.wixstatic.com/media/72c0b2_36d01136c39a4c1f8eef128d4413bdc4~mv2.png/v1/fill/w_1000,h_571,al_c,usm_0.66_1.00_0.01/72c0b2_36d01136c39a4c1f8eef128d4413bdc4~mv2.png"
+                alt="img1"
+              />
+              <li className="carouselSlide">
+                <img
+                  className="carouselImg"
+                  src="https://us.123rf.com/450wm/helgakhorimarko/helgakhorimarko1801/helgakhorimarko180100089/93833231-stock-vector-diverse-international-and-interracial-group-of-standing-women-we-are-stronger-together-text-for-girl.jpg?ver=6"
+                  alt="img2"
+                />
+              </li>
+            </li>
+            <li className="carouselSlide">
+              <img className="carouselImg" src={womenImg} alt="img3" />
+            </li>
+          </ul>
+        </div>
+        <div className="carouselBtns">
+          <button className="carouselBtn"> v </button>
+          <button className="carouselBtn"> v </button>
+        </div>
+      </div>
+      <div className="socialMediaIcons"></div>
     </div>
   );
 }
