1861d64dcb7f86795960f3967f4f7df2ce18cc80
Halit <halitfuatbatur@gmail.com>
2020-07-20 20:57:26 +0300

    removed extra imports in Home

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
 import Quotes from "../../components/quotes/Quotes";
 
 const Home = () => {
-  const { t } = useTranslation();
   return (
     <div>
-      <Quotes quotes={t("home.quotes")} />
+      <Quotes />
     </div>
   );
 };
