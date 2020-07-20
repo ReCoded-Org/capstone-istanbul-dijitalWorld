a0f9317bc02be48864499c534778ae568680bcf3
Halit <halitfuatbatur@gmail.com>
2020-07-20 16:22:59 +0300

    edit the layout sizes

--- src/components/quotes/Quotes.jsx
+++ src/components/quotes/Quotes.jsx
@@ -3,19 +3,20 @@ import { Col, Row, Container, Image } from "react-bootstrap";
 import safeImage from "../../images/safe_image.jpeg";
 import leonImage from "../../images/leon-biss.jpg";
 import kidsImage from "../../images/small-kids.png";
+import "./Quotes.css";
 
 const Quotes = () => {
   return (
     <Container>
       <Row>
         <Col lg={6} md={6} s={12}>
-          <div>
-            <h2>Say Her Name</h2>
+          <div className="content-container">
+            <h2>#SayHerName</h2>
           </div>
-          <div>
+          <div className="content-container">
             <Image src={leonImage} fluid />
           </div>
-          <div>
+          <div className="content-container">
             <h2>
               The fight for girls
               <br />
@@ -24,17 +25,17 @@ const Quotes = () => {
           </div>
         </Col>
         <Col lg={6} md={6} s={12}>
-          <div>
+          <div className="content-container">
             <Image src={safeImage} fluid />
           </div>
-          <div>
+          <div className="content-container">
             <h2>
               An Open Letter
               <br />
               to My Abuser
             </h2>
           </div>
-          <div>
+          <div className="content-container">
             <Image src={kidsImage} fluid />
           </div>
         </Col>
