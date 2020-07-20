4d7e5ef2033d22da978fb27c2faa6bbce8d16a05
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-07-20 18:48:47 +0300

    fixed prettier issue

--- src/components/inspire/Inspire.test.jsx
+++ src/components/inspire/Inspire.test.jsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import Inspire from "./Inspire";
 
 it("renders with no crash", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Inspire />, div)
+  const div = document.createElement("div");
+  ReactDOM.render(<Inspire />, div);
 });
