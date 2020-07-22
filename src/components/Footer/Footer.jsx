c4ca03f143e3e9df4644d4531ac3246a70c0daab
refica <refia.candan@gmail.com>
2020-07-22 23:56:56 +0300

    Renamed classes to be self-descriptive, used universal selector for common properties

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,26 +18,27 @@ const Footer = () => {
       <Container>
         <Row className="row">
           <Col className="footerCol" md="5" data-testid="col1">
-            <div className="divs">
-              <p className="title-left">{t("home.footer.getInTouch.title")}</p>
-              <p className="text-left">{t("home.footer.getInTouch.text")}</p>
-            </div>
+            <p className="footerBlurbTitle">
+              {t("home.footer.getInTouch.title")}
+            </p>
+            <p className="footerBlurb text">
+              {t("home.footer.getInTouch.text")}
+            </p>
           </Col>
 
           <Col className="footerCol" md="2" data-testid="col2">
-            <div className="divs">
-              <p className="title-mid">{t("home.footer.contactUs.title")}</p>
-              <p className="text-mid">
-                {t("home.footer.contactUs.emailAddress")}
-              </p>
-              <p className="text-mid">
-                {t("home.footer.contactUs.phoneNumber")}
-              </p>
-            </div>
+            <p className="footerContact title">
+              {t("home.footer.contactUs.title")}
+            </p>
+            <p className="footerContact text">
+              {t("home.footer.contactUs.emailAddress")}
+              <br />
+              {t("home.footer.contactUs.phoneNumber")}
+            </p>
           </Col>
 
           <Col className="footerCol" md="2" data-testid="col3">
-            <p className="title-right">{t("home.footer.followUs")}</p>
+            <p className="footerSocial title">{t("home.footer.followUs")}</p>
             <FontAwesomeIcon className="icon" icon={faTwitterSquare} />
             <FontAwesomeIcon className="icon" icon={faFacebook} />
             <FontAwesomeIcon className="icon" icon={faGithub} />
