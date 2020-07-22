943d30ad46c56d27723a8dfaf08f72c5335355a0
Dara <dara.dandashi@gmail.com>
2020-07-22 22:54:03 +0300

    rename test

--- src/components/carousel/Carousel.test.jsx
+++ src/components/carousel/Carousel.test.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Carousel from "./Carousel";
+import Carousel from "./Carousel.jsx";
 
-it("components are rendering well", () => {
+it("renders well", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Carousel />, div);
 });
