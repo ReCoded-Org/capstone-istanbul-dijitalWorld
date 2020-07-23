50b02956b667b9f82493c9887464ecc805c4bbae
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-07-24 00:40:56 +0300

    deleted extra test

--- src/components/inspire/Inspire.test.jsx
+++ src/components/inspire/Inspire.test.jsx
@@ -1,12 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Inspire from "./Inspire";
 import renderer from "react-test-renderer";
-
-it("renders with no crash", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Inspire />, div);
-});
 
 it("renders correctly", () => {
   const tree = renderer.create(<Inspire />).toJSON();
