ced3f953377105676a5bb2ace0c7ae50fc53e19b
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-07-23 23:09:17 +0300

    fixed tests to be snapshot and removed an extra line

--- src/components/inspire/Inspire.test.jsx
+++ src/components/inspire/Inspire.test.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Inspire from "./Inspire";
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import renderer from "react-test-renderer";
 
 it("renders with no crash", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Inspire />, div);
 });
-it("matches snapshot", () => {
-  const { getByTestId } = render(<Inspire />);
-  expect(getByTestId("inspireContainer")).toBeInTheDocument();
+
+it("renders correctly", () => {
+  const tree = renderer.create(<Inspire />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
