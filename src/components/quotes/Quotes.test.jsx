9d568d3abd0822be62d2088b82cd4438e147f9c3
Halit <halitfuatbatur@gmail.com>
2020-07-23 12:30:28 +0300

    update tests to snapshot and run yarn fix

--- src/components/quotes/Quotes.test.jsx
+++ src/components/quotes/Quotes.test.jsx
@@ -1,30 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import renderer from "react-test-renderer";
 import Quotes from "./Quotes";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Quotes />, div);
-});
-
-it("donate button renders correctly", () => {
-  const { getByTestId } = render(<Quotes />);
-  expect(getByTestId("button")).toHaveAttribute("type", "button");
-});
-
-it("say her name row is present in the document", () => {
-  const { getByTestId } = render(<Quotes />);
-  expect(getByTestId("sayHerNameRow")).toBeInTheDocument();
-});
-
-it("an open letter row is present in the document", () => {
-  const { getByTestId } = render(<Quotes />);
-  expect(getByTestId("anOpenLetterRow")).toBeInTheDocument();
-});
-
-it("fight for girls row is present in the document", () => {
-  const { getByTestId } = render(<Quotes />);
-  expect(getByTestId("fightForGirlsRow")).toBeInTheDocument();
+it("renders correctly", () => {
+  const tree = renderer.create(<Quotes />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
