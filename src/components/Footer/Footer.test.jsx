24d1053808de3327672ab740412e78b2b531e151
refica <refia.candan@gmail.com>
2020-07-24 23:25:28 +0300

    Ran prettier

--- src/components/Footer/Footer.test.jsx
+++ src/components/Footer/Footer.test.jsx
@@ -2,27 +2,7 @@ import React from 'react';
 import Footer from './Footer';
 import renderer from 'react-test-renderer';
 
-// it("Renders succesfully.", () => {
-//   const div = document.createElement("div");
-//   ReactDOM.render(<Footer />, div);
-// });
-
-// it("First column renders successfully.", () => {
-//   const { getByTestId } = render(<Footer />);
-//   expect(getByTestId("footerBlurbCol")).toBeInTheDocument();
-// });
-
-// it("Second column renders successfully.", () => {
-//   const { getByTestId } = render(<Footer />);
-//   expect(getByTestId("footerContactCol")).toBeInTheDocument();
-// });
-
-// it("Third column renders successfully.", () => {
-//   const { getByTestId } = render(<Footer />);
-//   expect(getByTestId("footerSocialCol")).toBeInTheDocument();
-// });
-
-it('Matches the snapshot.', () => {
+it("Matches the snapshot.", () => {
   const tree = renderer.create(<Footer />).toJSON();
   expect(tree).toMatchSnapshot();
 });
