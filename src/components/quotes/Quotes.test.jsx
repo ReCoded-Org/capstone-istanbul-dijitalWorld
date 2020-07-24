cda56fc61191833721b695e4c6c207419f6bc3c7
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-07-24 14:56:36 +0300

    Saving files before refreshing line endings

--- src/components/quotes/Quotes.test.jsx
+++ src/components/quotes/Quotes.test.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import '@testing-library/jest-dom';
-import Quotes from './Quotes';
 import renderer from 'react-test-renderer';
-        
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Quotes />, div);
+import Quotes from './Quotes';
 
 it('renders correctly', () => {
   const tree = renderer.create(<Quotes />).toJSON();
