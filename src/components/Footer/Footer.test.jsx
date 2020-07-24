aa8f9c9511559df364441731b3352a268d69fc78
refica <refia.candan@gmail.com>
2020-07-24 23:26:31 +0300

    Ran prettier

--- src/components/Footer/Footer.test.jsx
+++ src/components/Footer/Footer.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from './Footer';
 import renderer from 'react-test-renderer';
 
-it("Matches the snapshot.", () => {
+it('Matches the snapshot.', () => {
   const tree = renderer.create(<Footer />).toJSON();
   expect(tree).toMatchSnapshot();
 });
