ed8405a4700d1714716a8c69f88f75cea928f4df
Bahaa Dabbagh <62375331+bahaadabbagh@users.noreply.github.com>
2020-07-24 16:09:45 +0300

    InspireHome (#12)

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import Inspire from '../../components/inspire/Inspire';
+import { useTranslation } from 'react-i18next';
 import Quotes from '../../components/quotes/Quotes';
 
-const Home = () => {
+export default function Home() {
+  const { t } = useTranslation();
+
   return (
     <div>
       <Quotes />
+      <Inspire
+        header0={t('home.inspire.header.header0')}
+        header1={t('home.inspire.header.header1')}
+        header2={t('home.inspire.header.header2')}
+        header3={t('home.inspire.header.header3')}
+        heroText={t('home.inspire.heroText')}
+        button={t('home.inspire.button')}
+      />
     </div>
   );
-};
-
-export default Home;
+}
