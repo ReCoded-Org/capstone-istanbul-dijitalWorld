1efaf1b4986e577e830efbefff8d728ff6d4b933
Dara Aldandashi <62376204+Dara-D@users.noreply.github.com>
2020-07-26 14:22:25 +0300

    Carousel (#14)
    * added quote and learn more button with styling
    
    * added the carousel draft
    
    * added carousel
    
    * replaced px into rem
    
    * fixed social media icons
    
    * added trasnlation and fixed carousel animation
    
    * added test file, and tested it
    
    * deleted comments and ran prettier
    
    * pulled the current file with circleCI and changed variable names and class names and removed comments
    
    * run fix
    
    * rename test
    
    * fixed social media width from css
    
    * fixing test
    
    * fixing test
    
    * fixed prettier
    
    * fixed prettier
    
    * added snapshot and fixed errors
    
    * ran yarn fix on merged conflicts
    
    * fixed error  form conflict
    
    * deleted Carousel.jsx
    
    * added Carousel.jsx
    
    * Normalize all the line endings
    
    * ran yarn run fix
    
    * deleted Home.jsx
    
    * readded Home.jsx
    
    * fixed Carousel.css importaion and run fix and test
    
    * re pulled and merged
    
    * unified icons,edited span in css, removed bootstrap,  delete fontType , fixed translation and removed it from home
    
    * ran test
    
    * reinstalled yarn and ran test and fix
    
    * ran test and fix
    
    * pulled and re ran fix and test

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './carousel.css';
+import ControlledCarousel from './ControlledCarousel';
+import { useTranslation } from 'react-i18next';
+import {
+  faTwitterSquare,
+  faFacebookSquare,
+  faInstagramSquare,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function Carousel() {
+  const { t } = useTranslation();
+  return (
+    <div className="allContent">
+      <div className="sentenceAndButton">
+        <div className="sentence">
+          <p>
+            {t('home.carousel.header.header0')}
+            <br /> {t('home.carousel.header.header1')} <br />
+            {t('home.carousel.header.header2')} <span>{t('home.carousel.header.header3')}</span>
+          </p>
+        </div>
+        <button type="button" className="learnBtn">
+          {t('home.carousel.button')}
+        </button>
+      </div>
+      <div className="carouselImgs">
+        <ControlledCarousel />
+      </div>
+      <div className="socialMediaIcons">
+        <FontAwesomeIcon className="homeIcon" icon={faTwitterSquare} />
+        <FontAwesomeIcon className="homeIcon" icon={faFacebookSquare} />
+        <FontAwesomeIcon className="homeIcon" icon={faInstagramSquare} />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/carousel/ControlledCarousel.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import womenPhoto from './womenPhoto.png';
+import womenIllustration from './womenIllustration.png';
+import './carousel.css';
+
+function ControlledCarousel() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" src={womenPhoto} alt="First slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={womenIllustration} alt="Second slide" />
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default ControlledCarousel;
