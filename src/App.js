b238269c25c10bf7e3a3c80584518d4c949b1c93
Dara <dara.dandashi@gmail.com>
2020-07-30 01:15:25 +0300

    finalized AboutContent CSS

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Home from './containers/home/Home';
 import NavBar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
-// import About from './containers/about/About';
+import About from './containers/about/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <About />
       <Home />
       <Footer />
     </div>
