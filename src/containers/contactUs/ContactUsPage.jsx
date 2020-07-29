9ab0ca897e2d0aa5a44f994b30e9d8a08154e35b
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-07-29 20:19:41 +0300

    created files

---
+++ src/containers/contactUs/ContactUsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ContactUs from '../../components/contactUs/ContactUs'
+
+export default function ContactUsPage() {
+    return (
+        <div>
+            <ContactUs/>
+        </div>
+    )
+}
