5ea6c700227bfd7d690994f13424889273a64272
Halit <halitfuatbatur@gmail.com>
2020-07-29 17:10:53 +0300

    add initial boiler plate for the signup compoenent

---
+++ src/containers/signupPage/SignupPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignupForm from '../../components/signupForm/SignupForm';
+
+const signupPage = () => {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
+};
+
+export default signupPage;
