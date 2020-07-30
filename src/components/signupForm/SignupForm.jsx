9f2e4a99770c49991bd1d025782704e8e391c156
Halit <halitfuatbatur@gmail.com>
2020-07-30 13:32:22 +0300

    edit the from componenet and add css to button and logo

--- src/components/signupForm/SignupForm.jsx
+++ src/components/signupForm/SignupForm.jsx
@@ -1,63 +1,43 @@
 import React from 'react';
-import { Image, Form, Col, Button } from 'react-bootstrap';
+import { Image, Form, Col, Button, Container } from 'react-bootstrap';
 import Logo from '../../images/www-logo.png';
+import './SignupForm.css';
 
 const SignupForm = () => {
   return (
-    <div>
-      <Image src={Logo} fluid />
+    <Container className="signUp">
+      <Image src={Logo} className="logo" />
+      <h2>Signup Now</h2>
       <Form>
         <Form.Row>
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+          <Form.Group as={Col} controlId="formGridFirstName">
+            <Form.Control type="name" placeholder="FirstName" />
           </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+          <Form.Group as={Col} controlId="formGridLastName">
+            <Form.Control type="name" placeholder="LastName" />
           </Form.Group>
         </Form.Row>
 
-        <Form.Group controlId="formGridAddress1">
-          <Form.Label>Address</Form.Label>
-          <Form.Control placeholder="1234 Main St" />
-        </Form.Group>
-
-        <Form.Group controlId="formGridAddress2">
-          <Form.Label>Address 2</Form.Label>
-          <Form.Control placeholder="Apartment, studio, or floor" />
+        <Form.Group controlId="formGridEmail">
+          <Form.Control type="email" placeholder="Your Email" />
         </Form.Group>
 
         <Form.Row>
-          <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>City</Form.Label>
-            <Form.Control />
+          <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Control type="password" placeholder="Password" />
           </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State</Form.Label>
-            <Form.Control as="select" defaultValue="Choose...">
-              <option>Choose...</option>
-              <option>...</option>
-            </Form.Control>
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control />
+          <Form.Group as={Col} controlId="formGridRepeatPassword">
+            <Form.Control type="password" placeholder="Repeat Password" />
           </Form.Group>
         </Form.Row>
 
-        <Form.Group id="formGridCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
+        <Button variant="primary" type="submit" className="signupBtn">
+          Sign Up
         </Button>
       </Form>
-    </div>
+    </Container>
   );
 };
 
