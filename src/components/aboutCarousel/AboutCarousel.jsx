ba0904e3e57c4b75a393f6f8bbdd8026a85e363d
Dara <dara.dandashi@gmail.com>
2020-07-30 16:44:01 +0300

    added the team cards

--- src/components/aboutCarousel/AboutCarousel.jsx
+++ src/components/aboutCarousel/AboutCarousel.jsx
@@ -1,9 +1,83 @@
 import React from 'react';
+import './AboutCarousel.css';
+import Person from './images/actress.svg';
+import { useTranslation } from 'react-i18next';
+import { Col, Row, Container } from 'react-bootstrap';
 
 export default function AboutCarousel() {
+  const { t } = useTranslation();
   return (
-    <div>
-      <h2>hi carousel I am here</h2>
-    </div>
+    <Container className="AllCarouselContent">
+      <Row>
+        <Col>
+          <Row className="row">
+            <Col>
+              <img className="cardImg" src={Person} alt="" />
+            </Col>
+            <Col>
+              <h4>{t('about.name.3')}</h4>
+              <p>{t('about.tilte.3')}</p>
+            </Col>
+          </Row>
+        </Col>
+        <Col>
+          <Row className="row">
+            <Col>
+              <img className="cardImg" src={Person} alt="" />
+            </Col>
+            <Col>
+              <h4>{t('about.name.4')}</h4>
+              <p>{t('about.tilte.4')}</p>
+            </Col>
+          </Row>
+        </Col>
+        <Col>
+          <Row className="row">
+            <Col>
+              <img className="cardImg" src={Person} alt="" />
+            </Col>
+            <Col>
+              <h4>{t('about.name.5')}</h4>
+              <p>{t('about.tilte.5')}</p>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Row className="row">
+            <Col>
+              <img className="cardImg" src={Person} alt="" />
+            </Col>
+            <Col>
+              <h4>{t('about.name.0')}</h4>
+              <p>{t('about.tilte.0')}</p>
+            </Col>
+          </Row>
+        </Col>
+        <Col>
+          <Row className="row">
+            <Col>
+              <img className="cardImg" src={Person} alt="" />
+            </Col>
+            <Col>
+              <h4>{t('about.name.1')}</h4>
+              <p>{t('about.tilte.1')}</p>
+            </Col>
+          </Row>
+        </Col>
+        <Col>
+          <Row className="row">
+            <Col>
+              <img className="cardImg" src={Person} alt="" />
+            </Col>
+            <Col>
+              <h4>{t('about.name.2')}</h4>
+              <p>{t('about.tilte.2')}</p>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
