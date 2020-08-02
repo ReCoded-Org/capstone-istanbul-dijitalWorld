28527dd72168047f08a0ab995b34919037d9dcb4
Halit <halitfuatbatur@gmail.com>
2020-08-02 19:50:14 +0300

    import svgs for buttons and edit buttons

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Home from './containers/home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
+import LoginPage from './containers/loginPage/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar />
       <Home />
-      <Footer />
+      <Footer /> */}
+      <LoginPage />
     </div>
   );
 }
--- src/containers/loginPage/LoginPage.jsx
+++ src/containers/loginPage/LoginPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import LoginForm from '../../components/loginForm/LoginForm';
 
 const LoginPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
 
 export default LoginPage;
