95efa254a63bbd90ae68be17aa47499f57c0e5f2
Dara <dara.dandashi@gmail.com>
2020-08-04 20:17:31 +0300

    adding the team carousel when media change

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Home from './containers/home/Home';
 import NavBar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
+import About from './containers/about/About';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <About />
       <Home />
       <Footer />
     </div>
--- src/components/aboutCarousel/AboutCarousel.jsx
+++ src/components/aboutCarousel/AboutCarousel.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './AboutCarousel.css';
+import Carousel from 'react-bootstrap/Carousel';
 import teamMemeber0 from './images/girl.svg';
 import teamMemeber1 from './images/actress.svg';
 import teamMemeber2 from './images/doctor.svg';
@@ -11,78 +12,201 @@ import { Col, Row, Container } from 'react-bootstrap';
 
 export default function AboutCarousel() {
   const { t } = useTranslation();
+
   return (
-    <Container className="AllCarouselContent">
-      <Row>
-        <Col md>
-          <Row className="TeamCard">
-            <Col>
+    <div AllTeamsContent>
+      <Container className="Desktopview">
+        <Row>
+          <Col md>
+            <Row className="TeamCard">
+              <Col sm="4">
+                <img className="cardImg" src={teamMemeber0} alt="" />
+              </Col>
+              <Col md className="cardTexts">
+                <h4>{t('about.name.3')}</h4>
+                <p>{t('about.tilte.3')}</p>
+              </Col>
+            </Row>
+          </Col>
+          <Col md>
+            <Row className="TeamCard">
+              <Col sm="4">
+                <img className="cardImg" src={teamMemeber1} alt="" />
+              </Col>
+              <Col md className="cardTexts">
+                <h4>{t('about.name.4')}</h4>
+                <p>{t('about.tilte.4')}</p>
+              </Col>
+            </Row>
+          </Col>
+          <Col md>
+            <Row className="TeamCard">
+              <Col sm="4">
+                <img className="cardImg" src={teamMemeber2} alt="" />
+              </Col>
+              <Col md className="cardTexts">
+                <h4>{t('about.name.5')}</h4>
+                <p>{t('about.tilte.5')}</p>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col md>
+            <Row className="TeamCard">
+              <Col sm="4">
+                <img className="cardImg" src={teamMemeber3} alt="" />
+              </Col>
+              <Col md className="cardTexts">
+                <h4>{t('about.name.0')}</h4>
+                <p>{t('about.tilte.0')}</p>
+              </Col>
+            </Row>
+          </Col>
+          <Col md>
+            <Row className="TeamCard">
+              <Col sm="4">
+                <img className="cardImg" src={teamMemeber4} alt="" />
+              </Col>
+              <Col md className="cardTexts">
+                <h4>{t('about.name.1')}</h4>
+                <p>{t('about.tilte.1')}</p>
+              </Col>
+            </Row>
+          </Col>
+          <Col md>
+            <Row className="TeamCard">
+              <Col sm="4">
+                <img className="cardImg" src={teamMemeber5} alt="" />
+              </Col>
+              <Col md className="cardTexts">
+                <h4>{t('about.name.2')}</h4>
+                <p>{t('about.tilte.2')}</p>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container className="tabletView">
+        <Carousel>
+          <Carousel.Item>
+            <Row>
+              <Col className="TeamCardTabletView">
+                <img className="cardImg" src={teamMemeber0} alt="" />
+                <h4>{t('about.name.3')}</h4>
+                <p>{t('about.tilte.3')}</p>
+              </Col>
+              <Col>
+                <Row className="TeamCard">
+                  <Col>
+                    <img className="cardImg" src={teamMemeber1} alt="" />
+                  </Col>
+                  <Col className="cardTexts">
+                    <h4>{t('about.name.4')}</h4>
+                    <p>{t('about.tilte.4')}</p>
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                <Row className="TeamCard">
+                  <Col>
+                    <img className="cardImg" src={teamMemeber2} alt="" />
+                  </Col>
+                  <Col className="cardTexts">
+                    <h4>{t('about.name.5')}</h4>
+                    <p>{t('about.tilte.5')}</p>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Row>
+              <Col>
+                <Row className="TeamCard">
+                  <Col>
+                    <img className="cardImg" src={teamMemeber3} alt="" />
+                  </Col>
+                  <Col className="cardTexts">
+                    <h4>{t('about.name.0')}</h4>
+                    <p>{t('about.tilte.0')}</p>
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                <Row className="TeamCard">
+                  <Col>
+                    <img className="cardImg" src={teamMemeber4} alt="" />
+                  </Col>
+                  <Col className="cardTexts">
+                    <h4>{t('about.name.1')}</h4>
+                    <p>{t('about.tilte.1')}</p>
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                <Row className="TeamCard">
+                  <Col>
+                    <img className="cardImg" src={teamMemeber5} alt="" />
+                  </Col>
+                  <Col className="cardTexts">
+                    <h4>{t('about.name.2')}</h4>
+                    <p>{t('about.tilte.2')}</p>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
+
+      <Container className="phoneView">
+        <Carousel>
+          <Carousel.Item>
+            <div className="TeamCard">
               <img className="cardImg" src={teamMemeber0} alt="" />
-            </Col>
-            <Col className="cardTexts">
               <h4>{t('about.name.3')}</h4>
               <p>{t('about.tilte.3')}</p>
-            </Col>
-          </Row>
-        </Col>
-        <Col md>
-          <Row className="TeamCard">
-            <Col>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="TeamCard">
               <img className="cardImg" src={teamMemeber1} alt="" />
-            </Col>
-            <Col className="cardTexts">
               <h4>{t('about.name.4')}</h4>
               <p>{t('about.tilte.4')}</p>
-            </Col>
-          </Row>
-        </Col>
-        <Col md>
-          <Row className="TeamCard">
-            <Col>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="TeamCard">
               <img className="cardImg" src={teamMemeber2} alt="" />
-            </Col>
-            <Col className="cardTexts">
               <h4>{t('about.name.5')}</h4>
               <p>{t('about.tilte.5')}</p>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row>
-        <Col md>
-          <Row className="TeamCard">
-            <Col>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="TeamCard">
               <img className="cardImg" src={teamMemeber3} alt="" />
-            </Col>
-            <Col className="cardTexts">
               <h4>{t('about.name.0')}</h4>
               <p>{t('about.tilte.0')}</p>
-            </Col>
-          </Row>
-        </Col>
-        <Col md>
-          <Row className="TeamCard">
-            <Col>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="TeamCard">
               <img className="cardImg" src={teamMemeber4} alt="" />
-            </Col>
-            <Col className="cardTexts">
               <h4>{t('about.name.1')}</h4>
               <p>{t('about.tilte.1')}</p>
-            </Col>
-          </Row>
-        </Col>
-        <Col md>
-          <Row className="TeamCard">
-            <Col>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="TeamCard">
               <img className="cardImg" src={teamMemeber5} alt="" />
-            </Col>
-            <Col className="cardTexts">
               <h4>{t('about.name.2')}</h4>
               <p>{t('about.tilte.2')}</p>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
+    </div>
   );
 }
