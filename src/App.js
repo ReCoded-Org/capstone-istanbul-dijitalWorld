d727cb0a4eec9434a1257146021e7c2debb40ffd
Dara <dara.dandashi@gmail.com>
2020-08-04 13:27:04 +0300

    adding team images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from './containers/home/Home';
+// import Home from './containers/home/Home';
 import NavBar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
 import About from './containers/about/About';
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <NavBar />
       <About />
-      <Home />
+      {/* <Home /> */}
       <Footer />
     </div>
   );
--- src/components/aboutCarousel/AboutCarousel.jsx
+++ src/components/aboutCarousel/AboutCarousel.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './AboutCarousel.css';
-import Person from './images/actress.svg';
+import teamMemeber0 from './images/girl.svg';
+import teamMemeber1 from './images/actress.svg';
+import teamMemeber2 from './images/doctor.svg';
+import teamMemeber3 from './images/bussinessMan.svg';
+import teamMemeber4 from './images/callCenterAgent.svg';
+import teamMemeber5 from './images/dentist.svg';
 import { useTranslation } from 'react-i18next';
 import { Col, Row, Container } from 'react-bootstrap';
 
@@ -10,33 +15,33 @@ export default function AboutCarousel() {
     <Container className="AllCarouselContent">
       <Row>
         <Col>
-          <Row className="row">
+          <Row className="TeamCard">
             <Col>
-              <img className="cardImg" src={Person} alt="" />
+              <img className="cardImg" src={teamMemeber0} alt="" />
             </Col>
-            <Col>
+            <Col className="cardTexts">
               <h4>{t('about.name.3')}</h4>
               <p>{t('about.tilte.3')}</p>
             </Col>
           </Row>
         </Col>
         <Col>
-          <Row className="row">
+          <Row className="TeamCard">
             <Col>
-              <img className="cardImg" src={Person} alt="" />
+              <img className="cardImg" src={teamMemeber1} alt="" />
             </Col>
-            <Col>
+            <Col className="cardTexts">
               <h4>{t('about.name.4')}</h4>
               <p>{t('about.tilte.4')}</p>
             </Col>
           </Row>
         </Col>
         <Col>
-          <Row className="row">
+          <Row className="TeamCard">
             <Col>
-              <img className="cardImg" src={Person} alt="" />
+              <img className="cardImg" src={teamMemeber2} alt="" />
             </Col>
-            <Col>
+            <Col className="cardTexts">
               <h4>{t('about.name.5')}</h4>
               <p>{t('about.tilte.5')}</p>
             </Col>
@@ -45,33 +50,33 @@ export default function AboutCarousel() {
       </Row>
       <Row>
         <Col>
-          <Row className="row">
+          <Row className="TeamCard">
             <Col>
-              <img className="cardImg" src={Person} alt="" />
+              <img className="cardImg" src={teamMemeber3} alt="" />
             </Col>
-            <Col>
+            <Col className="cardTexts">
               <h4>{t('about.name.0')}</h4>
               <p>{t('about.tilte.0')}</p>
             </Col>
           </Row>
         </Col>
         <Col>
-          <Row className="row">
+          <Row className="TeamCard">
             <Col>
-              <img className="cardImg" src={Person} alt="" />
+              <img className="cardImg" src={teamMemeber4} alt="" />
             </Col>
-            <Col>
+            <Col className="cardTexts">
               <h4>{t('about.name.1')}</h4>
               <p>{t('about.tilte.1')}</p>
             </Col>
           </Row>
         </Col>
         <Col>
-          <Row className="row">
+          <Row className="TeamCard">
             <Col>
-              <img className="cardImg" src={Person} alt="" />
+              <img className="cardImg" src={teamMemeber5} alt="" />
             </Col>
-            <Col>
+            <Col className="cardTexts">
               <h4>{t('about.name.2')}</h4>
               <p>{t('about.tilte.2')}</p>
             </Col>
