3d5237817e23d352c1aa974935efd2a0c962b390
Halit <halitfuatbatur@gmail.com>
2020-08-05 14:56:09 +0300

    add array for routes and map throught the array

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import Home from './containers/home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './containers/home/Home';
 import NavBar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
+import InspirePage from './containers/inspirePage/InspirePage';
+
+// this array will be mapped throught to create the routes
+// Home component is a plceholder until other components are created
+
+const routes = [
+  { path: '/', name: 'Home', Component: Home },
+  { path: '/blog', name: 'Blog', Component: Home },
+  { path: '/inspire', name: 'inspire', Component: InspirePage },
+  { path: '/resources', name: 'Resources', Component: Home },
+  { path: '/about', name: 'About', Component: Home },
+  { path: '/contact', name: 'Contact', Component: Home },
+];
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Home />
+    <Router className="App">
+      <NavBar routes />
+      {routes.map(({ path, Component }) => (
+        <Route key={path} exact path={path}>
+          <Component />
+        </Route>
+      ))}
       <Footer />
-    </div>
+    </Router>
   );
 }
 
