1a3fd7f59d7248b3fc3b58427664498490e75606
Halit <halitfuatbatur@gmail.com>
2020-08-07 19:26:12 +0300

    add state for signup and handle inputs

--- src/components/signupForm/SignupForm.jsx
+++ src/components/signupForm/SignupForm.jsx
@@ -1,35 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image, Form, Col, Button, Container } from 'react-bootstrap';
 import Logo from '../../images/www-logo.png';
 import './SignupForm.css';
 
+const inputs = {
+  'First name': 'firstName',
+  'Last name': 'lastName',
+  'Your Email': 'email',
+  Password: 'password',
+  'Repeat password': 'repeatPassword',
+};
+
 const SignupForm = () => {
-  const handleSignup = (e) => {};
+  const [signupInfo, setSignupInfo] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
+  });
+
+  const handleSignup = () => {};
+
   return (
     <Container className="signupContainer">
       <Image src={Logo} className="signupLogo" />
       <Form onSubmit={handleSignup}>
         <Form.Row>
           <Form.Group as={Col} controlId="formGridFirstName" className="mt-3">
-            <Form.Control type="name" placeholder="First name" />
+            <Form.Control
+              type="name"
+              placeholder="First name"
+              value={signupInfo.firstName}
+              onChange={(e) => setSignupInfo({ ...signupInfo, firstName: e.target.value })}
+            />
           </Form.Group>
 
           <Form.Group as={Col} controlId="formGridLastName" className="mt-3">
-            <Form.Control type="name" placeholder="Last name" />
+            <Form.Control
+              type="name"
+              placeholder="Last name"
+              value={signupInfo.lastName}
+              onChange={(e) => setSignupInfo({ ...signupInfo, lastName: e.target.value })}
+            />
           </Form.Group>
         </Form.Row>
 
         <Form.Group controlId="formGridEmail" className="mt-3">
-          <Form.Control type="email" placeholder="Your email" />
+          <Form.Control
+            type="email"
+            placeholder="Your email"
+            value={signupInfo.email}
+            onChange={(e) => setSignupInfo({ ...signupInfo, email: e.target.value })}
+          />
         </Form.Group>
 
         <Form.Row>
           <Form.Group as={Col} controlId="formGridPassword" className="mt-3">
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={signupInfo.password}
+              onChange={(e) => setSignupInfo({ ...signupInfo, password: e.target.value })}
+            />
           </Form.Group>
 
           <Form.Group as={Col} controlId="formGridRepeatPassword" className="mt-3">
-            <Form.Control type="password" placeholder="Repeat password" />
+            <Form.Control
+              type="password"
+              placeholder="Repeat password"
+              value={signupInfo.repeatPassword}
+              onChange={(e) => setSignupInfo({ ...signupInfo, repeatPassword: e.target.value })}
+            />
           </Form.Group>
         </Form.Row>
 
