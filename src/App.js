64b62ce5d47c0406d8a96d59367b1964c0ba873f
Halit Batur <61846570+Halit-Batur@users.noreply.github.com>
2020-08-09 00:57:47 +0300

    Routing (#36)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import Home from './containers/home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
+import Home from './containers/home/Home';
 import NavBar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
+import InspirePage from './containers/inspirePage/InspirePage';
+
+// This array will be mapped through to create the routes
+// Home component is a placeholder until other components are created
+const ROUTES = [
+  { path: '/', name: 'home', Component: Home },
+  { path: '/blog', name: 'blog', Component: Home },
+  { path: '/inspire', name: 'inspire', Component: InspirePage },
+  { path: '/resources', name: 'resource', Component: Home },
+  { path: '/about', name: 'about', Component: Home },
+  { path: '/contact', name: 'contact', Component: Home },
+];
+
+const LocationDisplay = withRouter(({ location }) => (
+  <div data-testid="location-display">{location.pathname}</div>
+));
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Home />
+    <Router className="App">
+      <NavBar routes={ROUTES} />
+      {ROUTES.map(({ path, Component }) => (
+        <Route key={path} exact path={path}>
+          <Component />
+        </Route>
+      ))}
       <Footer />
-    </div>
+    </Router>
   );
 }
 
-export default App;
+export { App, LocationDisplay };
--- src/components/navbar/NavBar.test.jsx
+++ src/components/navbar/NavBar.test.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import NavBar from './NavBar';
 
+const routes = [
+  { path: '/', name: 'home' },
+  { path: '/blog', name: 'blog' },
+  { path: '/inspire', name: 'inspire' },
+  { path: '/resources', name: 'resource' },
+  { path: '/about', name: 'about' },
+  { path: '/contact', name: 'contact' },
+];
+
 it('Navbar renders correctly', () => {
-  const div = renderer.create(<NavBar />).toJSON();
+  const div = renderer.create(<NavBar routes={routes} />).toJSON();
   expect(div).toMatchSnapshot();
 });
