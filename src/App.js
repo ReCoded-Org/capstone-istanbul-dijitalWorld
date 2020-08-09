7fbbd401eaa53ae7dbf98ae2a4116fff7b9e232a
eliftabak <elifchorghay@gmail.com>
2020-08-09 19:51:48 +0300

    updated appjs

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './containers/home/Home';
 import NavBar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
-import BlogCard from './components/blog-cards/BlogCard';
 
 function App() {
   return (
     <Router className="App">
       <NavBar />
-      <BlogCard />
+      <Home />
       <Footer />
     </Router>
   );
--- src/components/single-article/SingleArticle.jsx
+++ src/components/single-article/SingleArticle.jsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react';
 import blogArticles from '../blog-cards/blog.json';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-
 function ScrollToTopOnMount() {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -20,16 +19,15 @@ export default function SingleArticle() {
     <div>
       <Switch>
         <ScrollToTopOnMount />
-        <Route path={`${url}/${id}`}>{
-          blogArticles.map((blog) => {
+        <Route path={`${url}/${id}`}>
+          {blogArticles.map((blog) => {
             return (
               <div key={blog.id}>
                 <h1>{blog.title}</h1>
                 <p>{blog.body}</p>
               </div>
-            )
-          }
-          )}
+            );
+          })}
         </Route>
       </Switch>
     </div>
