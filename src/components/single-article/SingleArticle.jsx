24078e72727ce2f633eeaff819c5eb1431ea377d
eliftabak <elifchorghay@gmail.com>
2020-08-10 00:19:45 +0300

    run yarn fix

--- src/components/single-article/SingleArticle.jsx
+++ src/components/single-article/SingleArticle.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import blogArticles from '../blog-cards/blog.json';
+import { Col, Row, Container, Image } from 'react-bootstrap';
 import './SingleArticle.css';
 
 function ScrollToTopOnMount() {
@@ -17,11 +18,20 @@ export default function SingleArticle() {
       <ScrollToTopOnMount />
       {blogArticles.map((blog) => {
         return (
-          <div key={blog.id} className="articleContainer">
-            <img src={blog.imgUrl} className="articleImage" />
-            <h1 className="articleTitle">{blog.title}</h1>
-            <span className="articleBody">{blog.body}</span>
-          </div>
+          <Container key={blog.id} className="articleContainer">
+            <Row>
+              <Col xs={6} md={6} lg={8}>
+                <h1 className="articleTitle">{blog.title}</h1>
+                <span>By {blog.author}</span>
+              </Col>
+              <Col xs={4} md={4} lg={3}>
+                <Image src={blog.imgUrl} className="articleImage" />
+              </Col>
+            </Row>
+            <Row>
+              <span className="articleBody">{blog.body}</span>
+            </Row>
+          </Container>
         );
       })}
     </div>
