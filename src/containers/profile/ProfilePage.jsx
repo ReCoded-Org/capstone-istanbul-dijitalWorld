9feffd08f53e3100b5e0d3b343d30467cc3ac399
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-08-11 21:50:14 +0300

    finished ProfilePage no functionality

---
+++ src/containers/profile/ProfilePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Profile from '../../components/profile/Profile';
+
+export default function profilePage() {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+}
