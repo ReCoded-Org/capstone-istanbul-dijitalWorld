56149840f29ca84a8c28f1504159a25c904bc568
eliftabak <elifchorghay@gmail.com>
2020-08-12 17:03:24 +0300

    fixed errors

--- src/components/blog-cards/BlogCard.jsx
+++ src/components/blog-cards/BlogCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Col, Row, Container, Image } from 'react-bootstrap';
-import { BrowserRouter as Router, NavLink } from 'react-router-dom';
+import { BrowserRouter as NavLink } from 'react-router-dom';
 import blogArticles from './blog.json';
 import './BlogCard.css';
 
@@ -13,18 +13,18 @@ export default function BlogCard() {
             <Col xs={6} md={6} lg={8}>
               <h3 className="blogTitle">{blog.title}</h3>
               <span className="blogBody">{blog.firstPart.substring(0, 250)}...</span>
-                <NavLink to={`/blog/${blog.id}`}>
-                  <button type="button" className="readMoreButton">
-                    Read More
-                  </button>
-                </NavLink>
+              <NavLink to={`/blog/${blog.id}`} key={blog.id}>
+                <button type="button" className="readMoreButton">
+                  Read More
+                </button>
+              </NavLink>
             </Col>
             <Col xs={4} md={4} lg={3} className="imageContainer">
               <Image src={blog.imgUrl} className="roundImage" roundedCircle />
             </Col>
           </Row>
         </Container>
-        <hr/>
+        <hr />
       </div>
     );
   });
--- src/components/single-article/SingleArticle.jsx
+++ src/components/single-article/SingleArticle.jsx
@@ -16,28 +16,30 @@ function ScrollToTopOnMount() {
 export default function SingleArticle({ match }) {
   let params = useParams();
   console.log(params.id);
-  
+
   return (
     <div>
       <ScrollToTopOnMount />
-      {blogArticles.filter(({id}) => id === parseInt(params.id)).map((blog) => {
-        return (
-          <Container key={blog.id} className="articleContainer">
-            <Row>
-              <Col xs={6} md={6} lg={8}>
-                <h1 className="articleTitle">{blog.title}</h1>
-                <span>By {blog.author}</span>
-              </Col>
-            </Row>
-            <Row>
-              <span className="articleBody">{blog.firstPart}</span>
-              <Image src={blog.imgUrl} className="articleImage" />
-              <span className="articleBody">{blog.secondPart}</span>
-              <span className="articleBody">{blog.thirdPart}</span>
-            </Row>
-          </Container>
-        );
-      })}
+      {blogArticles
+        .filter(({ id }) => id === parseInt(params.id))
+        .map((blog) => {
+          return (
+            <Container key={blog.id} className="articleContainer">
+              <Row>
+                <Col xs={6} md={6} lg={8}>
+                  <h1 className="articleTitle">{blog.title}</h1>
+                  <span>By {blog.author}</span>
+                </Col>
+              </Row>
+              <Row>
+                <span className="articleBody">{blog.firstPart}</span>
+                <Image src={blog.imgUrl} className="articleImage" />
+                <span className="articleBody">{blog.secondPart}</span>
+                <span className="articleBody">{blog.thirdPart}</span>
+              </Row>
+            </Container>
+          );
+        })}
     </div>
   );
 }
