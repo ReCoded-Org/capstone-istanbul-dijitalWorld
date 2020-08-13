447c48511f0cb289df47ac76e7d006d505ce9c31
Halit Batur <61846570+Halit-Batur@users.noreply.github.com>
2020-08-13 11:34:37 +0300

    Signup auth (#39)
    * import auth and change imports in inspre and contactus page
    
    * add state for signup and handle inputs
    
    * add required for the form controls
    
    * handle signup
    
    * add alert message on error
    
    * add a success alert for signup
    
    * add redirecting to home after success signup
    
    * add repeat password validation
    
    * update snapshot test
    
    * run yarn fix
    
    * add signup route
    
    * send a verification email to new users
    
    * yarn run fix and update snapshot test
    
    * fix typos, add handle change function and remove change alert function
    
    * fix comments

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SingleArticle from './components/single-article/SingleArticle';
 import Footer from './components/footer/Footer';
 import InspirePage from './containers/inspirePage/InspirePage';
 import About from './containers/about/About';
+import SignupPage from './containers/signupPage/SignupPage';
 
 // This array will be mapped through to create the routes
 // Home component is a placeholder until other components are created
@@ -33,7 +34,13 @@ function App() {
           <Component />
         </Route>
       ))}
+
+      <Route exact path="/signup">
+        <SignupPage />
+      </Route>
+
       <Route exact path="/blog/:id" render={(props) => <SingleArticle {...props} />} />
+
       <Footer />
     </Router>
   );
--- src/components/signupForm/SignupForm.jsx
+++ src/components/signupForm/SignupForm.jsx
@@ -1,34 +1,112 @@
-import React from 'react';
-import { Image, Form, Col, Button, Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Image, Form, Col, Button, Container, Alert } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
+import { auth } from '../../firebase';
 import Logo from '../../images/www-logo.png';
 import './SignupForm.css';
 
 const SignupForm = () => {
+  // This state manages the redirecting to the home page that happens after a successful signup
+  const [redirect, setRedirect] = useState(false);
+  // Alert is null if not present. If present, has two keys, message and status.
+  // Status's value will decide the appearance of the alert
+  const [alert, setAlert] = useState(null);
+  const [signupInfo, setSignupInfo] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
+  });
+
+  // This function uses firebase auth to register the user
+  // In case of any error it will change the alert state to render a component that shows an error message to the user
+  // If the login was successful it will show  a success alert message and then redirect the user to the home page
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    if (signupInfo.password !== signupInfo.repeatPassword) {
+      setAlert({ message: "Passwords doesn't match", status: 'danger' });
+      return;
+    }
+    try {
+      const response = await auth.createUserWithEmailAndPassword(
+        signupInfo.email,
+        signupInfo.password,
+      );
+      response.user.sendEmailVerification();
+    } catch (error) {
+      setAlert({ message: error.message, status: 'danger' });
+      return;
+    }
+    setSignupInfo({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      repeatPassword: '',
+    });
+    setAlert({ message: 'Your registration is completed!', status: 'success' });
+    setTimeout(() => setRedirect(true), 1000);
+  };
+
+  const handleChange = (key) => (e) => {
+    setSignupInfo({ ...signupInfo, [key]: e.target.value });
+  };
+
   return (
     <Container className="signupContainer">
       <Image src={Logo} className="signupLogo" />
-      <Form>
+      <Form onSubmit={handleSignup} className="form">
         <Form.Row>
           <Form.Group as={Col} controlId="formGridFirstName" className="mt-3">
-            <Form.Control type="name" placeholder="First name" />
+            <Form.Control
+              required
+              type="name"
+              placeholder="First name"
+              value={signupInfo.firstName}
+              onChange={handleChange('firstName')}
+            />
           </Form.Group>
 
           <Form.Group as={Col} controlId="formGridLastName" className="mt-3">
-            <Form.Control type="name" placeholder="Last name" />
+            <Form.Control
+              required
+              type="name"
+              placeholder="Last name"
+              value={signupInfo.lastName}
+              onChange={handleChange('lastName')}
+            />
           </Form.Group>
         </Form.Row>
 
         <Form.Group controlId="formGridEmail" className="mt-3">
-          <Form.Control type="email" placeholder="Your email" />
+          <Form.Control
+            type="email"
+            placeholder="Your email"
+            value={signupInfo.email}
+            onChange={handleChange('email')}
+          />
         </Form.Group>
 
         <Form.Row>
           <Form.Group as={Col} controlId="formGridPassword" className="mt-3">
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              required
+              type="password"
+              placeholder="Password"
+              value={signupInfo.password}
+              onChange={handleChange('password')}
+            />
           </Form.Group>
 
           <Form.Group as={Col} controlId="formGridRepeatPassword" className="mt-3">
-            <Form.Control type="password" placeholder="Repeat password" />
+            <Form.Control
+              required
+              type="password"
+              placeholder="Repeat password"
+              value={signupInfo.repeatPassword}
+              onChange={handleChange('repeatPassword')}
+            />
           </Form.Group>
         </Form.Row>
 
@@ -36,6 +114,12 @@ const SignupForm = () => {
           Sign Up
         </Button>
       </Form>
+      {alert && (
+        <Alert className="mt-3" variant={alert.status} onClose={() => setAlert(null)} dismissible>
+          <p>{alert.message}</p>
+        </Alert>
+      )}
+      {redirect && <Redirect to="/" />}
       <p className="mt-3">
         Already Have An Account ?{' '}
         <a href="/login" className="loginLink">
