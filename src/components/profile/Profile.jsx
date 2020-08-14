40760e72a36671d296b286cef3713f74301f48f8
halit <halitfuatbatur@gmail.com>
2020-08-14 23:51:05 +0300

    added update user Data function

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Profile.css';
 import { Container, Col, Row, Image } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import deleteThis from '../../images/stupidGuy.png';
+import { useSelector } from 'react-redux';
+import { auth } from "../../firebase";
 
 export default function Profile() {
+  // const dispatch = useDispatch();
+  const userData = useSelector((state) => state.currentUserDataReducer)
+  console.log(userData)
   const { t } = useTranslation();
+
+  const updateUserData = async () => {
+    console.log(auth.currentUser)
+    const response = await auth.currentUser.updateProfile({
+      displayName: "Jennet Hydyrova",
+      birthday: "20-20-2019"
+    })
+    console.log(response)
+  }
 
   return (
     <Container>
       <Col md="12">
         <Row className="profileImageRow">
-          <Image src={deleteThis} alt="profileImage" roundedCircle />
+          <Image onClick={updateUserData} src={deleteThis} alt="profileImage" roundedCircle />
         </Row>
         <div className="profileDetailRow">
           <Row>
             <Col>
               <h5>{t('profile.fullName')}</h5>
             </Col>
-            <Col> name surname</Col>
+            <Col>{userData && userData.displayName}</Col>
           </Row>
           <Row>
             <Col>
               <h5>{t('profile.email')}</h5>
             </Col>
-            <Col> email@gmail.com</Col>
+            <Col> {userData && userData.email}</Col>
           </Row>
           <Row>
             <Col>
