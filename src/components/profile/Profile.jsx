ece67781f3b541f139d29dd41d10f275156ea34a
halit <halitfuatbatur@gmail.com>
2020-08-15 00:55:51 +0300

    get data from redux and use it for the fields in the profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,13 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import './Profile.css';
 import { Container, Col, Row, Image } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import deleteThis from '../../images/stupidGuy.png';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { userLoggedInAction } from '../../redux/action';
 import { auth } from "../../firebase";
 
 export default function Profile() {
-  // const dispatch = useDispatch();
+  const [formUserInfo, setUserInfo] = useState({
+    displayName: "",
+    phoneNumber: "",
+
+  })
+  const dispatch = useDispatch();
   const userData = useSelector((state) => state.currentUserDataReducer)
   console.log(userData)
   const { t } = useTranslation();
@@ -15,17 +21,18 @@ export default function Profile() {
   const updateUserData = async () => {
     console.log(auth.currentUser)
     const response = await auth.currentUser.updateProfile({
-      displayName: "Jennet Hydyrova",
+      displayName: "omegaaaaalul",
       birthday: "20-20-2019"
     })
-    console.log(response)
+    dispatch(userLoggedInAction(auth.currentUser));
+
   }
 
   return (
-    <Container>
+    <Container className="mt-3">
       <Col md="12">
         <Row className="profileImageRow">
-          <Image onClick={updateUserData} src={deleteThis} alt="profileImage" roundedCircle />
+          <Image onClick={updateUserData} style={{ width: "200px" }} src={userData && userData.photoURL} alt="profileImage" />
         </Row>
         <div className="profileDetailRow">
           <Row>
@@ -42,15 +49,15 @@ export default function Profile() {
           </Row>
           <Row>
             <Col>
-              <h5>{t('profile.birthday')}</h5>
+              <h5>{t('profile.phone')}</h5>
             </Col>
-            <Col>31-13-1901</Col>
+            <Col>{userData && (userData.phoneNumber ? userData.phoneNumber : "No phone number saved")} </Col>
           </Row>
           <Row>
             <Col>
-              <h5>{t('profile.phone')}</h5>
+              <h5>{t('profile.verified')}</h5>
             </Col>
-            <Col>+811234567 </Col>
+            <Col>{userData && userData.emailVerified ? "Email verified!" : "Email is not verified"} </Col>
           </Row>
         </div>
       </Col>
