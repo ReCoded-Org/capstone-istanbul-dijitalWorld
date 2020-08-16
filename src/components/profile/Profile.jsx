f331f14fe6eaeefe621bc78b2eb63078985f0d25
Halit Batur <61846570+Halit-Batur@users.noreply.github.com>
2020-08-16 21:44:27 +0300

    Dynamic profile page (#54)

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,45 +1,140 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Profile.css';
-import { Container, Col, Row, Image } from 'react-bootstrap';
+import { Container, Col, Row, Image, Modal, Button, Form, Alert } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import deleteThis from '../../images/stupidGuy.png';
+import { useSelector, useDispatch } from 'react-redux';
+import { userLoggedInAction } from '../../redux/action';
+import { auth } from '../../firebase';
+import { ANONYMOUS_PHOTO_URL } from '../../images/anonymous';
 
 export default function Profile() {
+  const currentUser = auth.currentUser;
+  // Alert state would be changed into an object with 2 keys
+  // First key will be the message the alert would display
+  // Second key will be the color of the alert success for green danger for red
+  const [alert, setAlert] = useState(null);
+  const [formUserInfo, setFormUserInfo] = useState({
+    displayName: '',
+    email: '',
+  });
+  const [showModal, setShowModal] = useState(false);
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.currentUserDataReducer);
   const { t } = useTranslation();
 
+  const updateUserData = async () => {
+    if (formUserInfo.displayName) {
+      await currentUser.updateProfile({
+        displayName: formUserInfo.displayName,
+      });
+    }
+    if (formUserInfo.email) {
+      await currentUser.updateEmail(formUserInfo.email);
+    }
+    // updating my state after the user changes his name or email
+    dispatch(userLoggedInAction(currentUser));
+    setFormUserInfo({
+      displayName: '',
+      email: '',
+    });
+    setAlert({
+      message: 'Profile updated successfully',
+      status: 'success',
+    });
+  };
+  // All the data that is coming from the userData redux state is checked incase it's null
+  // Because when the component render userData default value starts as null which caused an error if I try and use userData.photoUrl
   return (
-    <Container>
+    <Container className="mt-3">
       <Col md="12">
         <Row className="profileImageRow">
-          <Image src={deleteThis} alt="profileImage" roundedCircle />
+          <Image
+            onClick={updateUserData}
+            style={{ width: '12.5rem' }}
+            src={userData && userData.photoURL ? userData.photoURL : ANONYMOUS_PHOTO_URL}
+            alt="profileImage"
+            roundedCircle
+          />
+
+          <Button className="mt-3" onClick={() => setShowModal(true)}>
+            Edit Profile
+          </Button>
+
+          {alert && (
+            <Alert
+              className="mt-3"
+              variant={alert.status}
+              onClose={() => setAlert(null)}
+              dismissible
+            >
+              <p>{alert.message}</p>
+            </Alert>
+          )}
         </Row>
+
         <div className="profileDetailRow">
           <Row>
             <Col>
               <h5>{t('profile.fullName')}</h5>
             </Col>
-            <Col> name surname</Col>
+            <Col>{userData && userData.displayName}</Col>
           </Row>
           <Row>
             <Col>
               <h5>{t('profile.email')}</h5>
             </Col>
-            <Col> email@gmail.com</Col>
+            <Col> {userData && userData.email}</Col>
           </Row>
           <Row>
             <Col>
-              <h5>{t('profile.birthday')}</h5>
+              <h5>{t('profile.verified')}</h5>
             </Col>
-            <Col>31-13-1901</Col>
-          </Row>
-          <Row>
             <Col>
-              <h5>{t('profile.phone')}</h5>
+              {userData && userData.emailVerified ? 'Email verified!' : 'Email is not verified'}
             </Col>
-            <Col>+811234567 </Col>
           </Row>
         </div>
       </Col>
+      <Modal
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">Change Profile Info </Modal.Title>
+        </Modal.Header>
+        <Form
+          onSubmit={(e) => {
+            e.preventDefault();
+            updateUserData();
+            setShowModal(false);
+          }}
+        >
+          <Modal.Body>
+            <Form.Group controlId="formBasicName">
+              <Form.Control
+                type="name"
+                placeholder="name"
+                value={formUserInfo.displayName}
+                onChange={(e) => setFormUserInfo({ ...formUserInfo, displayName: e.target.value })}
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                value={formUserInfo.email}
+                onChange={(e) => setFormUserInfo({ ...formUserInfo, email: e.target.value })}
+              />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button type="submit">Submit changes</Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
     </Container>
   );
 }
