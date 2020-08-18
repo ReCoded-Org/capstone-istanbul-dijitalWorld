b31d1c6844779a91976e844aa6e689e3ea7f84ed
halit <halitfuatbatur@gmail.com>
2020-08-18 17:14:10 +0300

    update snapshot

--- src/components/navbar/NavLinks.jsx
+++ src/components/navbar/NavLinks.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from "react-router-dom"
+import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import './NavBar.css';
