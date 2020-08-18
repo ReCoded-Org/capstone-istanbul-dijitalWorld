9ebe465e7bceade423a360688a1886aa3e6aa5ee
Elif Tabak <58824027+eliftabak@users.noreply.github.com>
2020-08-18 18:18:17 +0300

    Code bash elif (#63)
    * Fixed Issues
    
    fixed en.json structure.
    fixed navbar look.
    
    * added blog card component
    
    * merged with new updated from dev
    
    * passed test
    
    * restore lost commits
    
    * Update BlogCard.jsx
    
    * passed test
    
    * added initial files and rebase blog-cards branch
    
    * added a button
    
    * added a button
    
    * finished designing and add a test
    
    * run fix
    
    * updated button
    
    * import navlinks and add router
    
    * updated test
    
    * updated
    
    * updated appjs
    
    * finished blog card
    
    * removed errors
    
    * run yarn fix
    
    * edited image names
    
    * updated the test
    
    * small changes
    
    * change folder name to blogcards
    
    * fixed failed test
    
    * merge with blog branch
    
    * routing is working
    
    * before checkout
    
    * finally finished
    
    * created a new branch for styling
    
    * connected blog articles to wordpress
    
    * fix failed test
    
    * fixed unwanted characters
    
    * added test
    
    * fix test
    
    * request changes made
    
    * changes
    
    * brand logo directs home
    
    * made requested changes
    
    Co-authored-by: Halit <halitfuatbatur@gmail.com>

--- src/components/blog-cards/BlogCard.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import BlogCard from './BlogCard.jsx';
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<BlogCard />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
