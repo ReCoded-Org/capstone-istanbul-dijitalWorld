0c3bc9aafc08f06eb87ff1a4246be2967dc46b56
eliftabak <elifchorghay@gmail.com>
2020-08-19 14:14:43 +0300

    fixed a problem

--- src/components/blog/blogCards/SingleBlogCard.jsx
+++ src/components/blog/blogCards/SingleBlogCard.jsx
@@ -9,7 +9,7 @@ export default function SingleBlogCard(props) {
   return (
     <div className="containerToAllCards">
       <Container className="blogContainer">
-        <Row>
+        <Row className="blogCardBox">
           <Col xs={4} md={6} lg={8}>
             <h3
               className="blogTitle"
